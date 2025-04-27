16193bd5371b7764032636712a204662638c2d0d
lmmddb <mamadou.djau8@gmail.com>
2025-04-27 14:52:00 +0200

    Add files via upload

--- DEFAULTConfig.py
+++ DEFAULTConfig.py
@@ -1,8 +1,16 @@
+import os
 import configparser
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-DISCORD_OWNER_ID=config['DEFAULT']['discord_owner_id']
-DISCORD_SDK=config['DEFAULT']['discord_sdk']
-GEMINI_SDK=config['DEFAULT']['gemini_sdk'] 
+# Récupération des valeurs
+DISCORD_OWNER_ID = config['DEFAULT'].get('discord_owner_id')
+DISCORD_SDK = os.environ.get('DISCORD_SDK')  # Variable Render
+GEMINI_SDK = os.environ.get('GEMINI_SDK')    # Variable Render
+
+# Vérifications
+if not DISCORD_SDK:
+    raise ValueError("DISCORD_SDK manquant. Configurez-le sur Render !")
+if not GEMINI_SDK:
+    raise ValueError("GEMINI_SDK manquant. Configurez-le sur Render !")
