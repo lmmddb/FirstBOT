6c709bb9ca64623986074d917b339f8ede1cf5cc
lmmddb <mamadou.djau8@gmail.com>
2025-04-28 11:04:07 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__)))
 
-import configs.DEFAULTConfig as defaultconfig
+import DEFAULTConfig as defaultconfig
 import DiscordUtil as discordUtil
 
 import discord
